fe7b100bfd8274c1ad7d8f0224bb175a3fab8fb1
jorge robles <jorge.diego.robles@gmail.com>
2026-07-17 16:12:13 +0200

    fix: build info uses dynamic git hash

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
+import { execSync } from "child_process";
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 export default defineConfig({
   base: '/orient-stl/',
   plugins: [wasm()],
   define: {
-    __COMMIT_HASH__: JSON.stringify('f5a343b'),
+    __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   build: {
     target: "esnext",
